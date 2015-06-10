7b4f489f096b31a5377e8677c88e9a9abe773c5e
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-10 16:57:18 -0400

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Teacher.create!(first_name: 'Edna', last_name: 'Krabappel', email: 'ekrabappel@example.com', password: 'pa55word')
+Teacher.create!(first_name: 'David', last_name: 'Wright', email: 'mets@example.com', password: 'pa55word')
+Teacher.create!(first_name: 'Semour', last_name: 'Skinner', email: 'sskinner@example.com', password: 'pa55word')
+
+50.times do
+  Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'pa55word', teacher_id = 1)
+end
+
+50.times do
+  Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'pa55word', teacher_id = 2)
+end
+
+50.times do
+  Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'pa55word', teacher_id = 3)
+end
+
+50.times do |count|
+  Parent.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'pa55word', student_id = count)
+end
