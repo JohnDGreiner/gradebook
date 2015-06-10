710f12e17e6ea3d07066efaec1c65b30b6bc58b3
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-10 17:10:57 -0400

    Correct seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Teacher.create!(first_name: 'Edna', last_name: 'Krabappel', email: 'ekrabappel@example.com', password: 'pa55word')
-Teacher.create!(first_name: 'David', last_name: 'Wright', email: 'mets@example.com', password: 'pa55word')
-Teacher.create!(first_name: 'Semour', last_name: 'Skinner', email: 'sskinner@example.com', password: 'pa55word')
+Teacher.create!(first_name: 'Edna', last_name: 'Krabappel', email: 'ekrabappel@example.com', password_digest: 'pa55word')
+Teacher.create!(first_name: 'David', last_name: 'Wright', email: 'mets@example.com', password_digest: 'pa55word')
+Teacher.create!(first_name: 'Semour', last_name: 'Skinner', email: 'sskinner@example.com', password_digest: 'pa55word')
 
 50.times do
-  Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'pa55word', teacher_id = 1)
+  Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password_digest: 'pa55word', teacher_id: 1)
 end
 
 50.times do
-  Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'pa55word', teacher_id = 2)
+  Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password_digest: 'pa55word', teacher_id: 2)
 end
 
 50.times do
-  Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'pa55word', teacher_id = 3)
+  Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password_digest: 'pa55word', teacher_id: 3)
 end
 
 50.times do |count|
-  Parent.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'pa55word', student_id = count)
+  Parent.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password_digest: 'pa55word', student_id: count)
 end
